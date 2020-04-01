45726f876fa8c40fc08a279b80b261ccc96ff530
Tasari <Tasari@wp.pl>
2020-04-01 15:46:37 +0200

    Added comments of functions in add_to_db.py

--- add_to_db.py
+++ add_to_db.py
@@ -7,6 +7,9 @@ from base_template import Session, engine, Base
 Base.metadata.create_all(engine)
 
 def add_to_stock(name, cost):
+    '''
+    Function adding to stock database
+    '''
     session = Session()
     name = name_changer(name)
     if is_in_stock(name, session):
@@ -17,11 +20,17 @@ def add_to_stock(name, cost):
     return 0
 
 def is_in_stock(name, session):
+    '''
+    Function checking if object with given name is in stock
+    '''
     if session.query(Products).filter(Stock.name == name).all() != []:
         return 1
     return 0
 
 def string_to_object_from_db(name, db, session):
+    '''
+    Function returning object with given name from database
+    '''
     ingredient_obj = session.query(db).filter(db.name == name).all()
     if ingredient_obj == []:
         return 0
@@ -32,7 +41,9 @@ def add_product(name, cost, recipe):
     Recipe has to be a list of lists, 
     where 1st element is the name of item in stock 
     and second is the amount of given ingredient
-    Valid use add_product(name, cost, [['ingredient1', amount_of_ingredient1], ['ingredient2', amount_of_ingredient2]])
+    Valid use: add_product(name, 
+                cost, 
+                [['ingredient1', amount_of_ingredient1], ['ingredient2', amount_of_ingredient2]])
     '''
     session = Session()
     name = name_changer(name)
@@ -46,11 +57,18 @@ def add_product(name, cost, recipe):
     return 0
 
 def is_a_product(name, session):
+    '''
+    Checks if name is in products database
+    '''
     if session.query(Products).filter(Products.name == name).all() != []:
         return 1
     return 0
 
 def name_changer(name):
+    '''
+    Function changing all words in given string 
+    to have 1st letter capitalized and next small 
+    '''
     new_name = ''
     words_in_name = name.split()
     for word in words_in_name:
