e686ff194b0f841a946c59030c47688b74f3e9f1
Tasari <Tasari@wp.pl>
2020-04-01 16:07:32 +0200

    Added restock

--- stock.py
+++ stock.py
@@ -16,3 +16,21 @@ class Stock(Base):
 
     def __repr__(self):
         return self.name
+
+    def restock(self, amount, mode='add'):
+        '''
+        restock(amount) - adds given amount to stock
+        restock(amount, mode='set' - sets given amount in stock)
+        '''
+        if mode == 'add':
+            if self.quantity + amount >= 0:
+                self.quantity += amount
+            else:
+                return -1
+        elif mode == 'set':
+            if amount >= 0:
+                self.quantity = amount
+            else:
+                return -2
+        else:
+            return -3
