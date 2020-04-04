dbd8fd61a2ead018be770d9ea80d03d5787f181b
Tasari <Tasari@wp.pl>
2020-04-04 14:12:50 +0200

    Added order_prod database

---
+++ order_product.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+from base_template import Base
+
+class Order_Product(Base):
+    __tablename__ = 'order_product'
+    
+    product_id = Column(Integer, ForeignKey('products.id'), primary_key=True)
+    order_id = Column(Integer, ForeignKey('order.id'), primary_key = True)
+    ordered_product = relationship('Products')
+    amount = Column(Integer)
+
+    def __init__(self, ordered_product, amount):
+        self.amount = amount
+        self.ordered_product = ordered_product
+
+    def __repr__(self):
+        return self.ordered_product.__repr__()
