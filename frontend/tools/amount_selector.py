f4d46cd416a6aa5ec3acff992a0b771c84b60301
Tasari <Tasari@wp.pl>
2020-05-08 20:50:31 +0200

    Added button functions in amount selector

--- frontend/tools/amount_selector.py
+++ frontend/tools/amount_selector.py
@@ -6,15 +6,25 @@ class AmountSelector(tk.Frame):
         self.minus_image = tk.PhotoImage(file = ".\images\minus.png")
         self.plus_image = tk.PhotoImage(file = ".\images\plus.png")
         self.item_name = tk.Label(self, borderwidth=1, text=item_name, font = ("Courier", 15))
-        self.subtract_button = tk.Button(self, image=self.minus_image)
-        self.entry = tk.Entry(self, font = ("Courier", 35), width=2)
-        self.add_button = tk.Button(self, image = self.plus_image)
+        self.subtract_button = tk.Button(self, image=self.minus_image, command = self.subtract_one)
+        self.entry = tk.Entry(self, font = ("Courier", 35), width=5)
+        self.add_button = tk.Button(self, image = self.plus_image, command = self.add_one)
+        self.entry.insert(0, "0")
 
         self.item_name.grid(column=0, row=0)
         self.subtract_button.grid(column=1, row=0)
         self.entry.grid(column=2, row=0)
         self.add_button.grid(column=3, row=0)
 
+    def add_one(self):
+        amount = int(self.entry.get())+1
+        self.entry.delete(0, tk.END)
+        self.entry.insert(0, str(amount))
+
+    def subtract_one(self):
+        amount = int(self.entry.get())-1
+        self.entry.delete(0, tk.END)
+        self.entry.insert(0, str(amount))
 
     def get_entry(self):
         return self.entry.get()
