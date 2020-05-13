c0bbce3e3b4e752f071605300bb36568c932eb3c
Tasari <Tasari@wp.pl>
2020-05-13 16:45:00 +0200

    Created preorder object in order creation

--- frontend/order/order_creation.py
+++ frontend/order/order_creation.py
@@ -1,21 +1,32 @@
-from tkinter import Button, Toplevel, Label, Entry, Frame, Scrollbar, Canvas
 from tools import get_all_items_from_table
 from tables.products import Product
-from frontend.tools.amount_selector import AmountSelector
+from tables.order import Order
 
-def order_creation():
-    items = get_all_items_from_table(Product)
-    ordered_items = []
-    new_order_window = Toplevel()
-    new_order_window.title("New order")
-    test = AmountSelector(new_order_window, 'test')
-    test.pack()
-    return
+class PreOrder():
+    def __init__(self):
+        self.possible_to_order = get_all_items_from_table(Product)
+        self.ordered = []
 
-def add_product_to_order():
-    return
-def remove_product_from_order(master):
-    return
+    def add_to_ordered(self, product):
+        try:
+            assert product in self.possible_to_order
+            self.possible_to_order.remove(product)
+            self.ordered.append(product)
+        except AssertionError:
+            print("That product is not possible to order")
 
-def onFrameConfigure(das):
-    return
+    def remove_from_ordered(self, product):
+        try:
+            assert product in self.ordered
+            self.ordered.append(product)
+            self.possible_to_order.remove(product)
+
+        except AssertionError:
+            print("That product is not ordered")
+
+    def finish_preorder(self, amount_selectors_list, wallet, user):
+        final_order = Order()
+        for amount_selector in amount_selectors_list:
+            final_order.add_product_to_order(amount_selector.item, amount_selector.get_entry())
+        user.finish_order(wallet, final_order)
+
