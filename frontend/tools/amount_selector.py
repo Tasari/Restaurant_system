94625cd4055eafebdaa5067857e51d8b98acd9a3
Tasari <Tasari@wp.pl>
2020-05-13 16:11:41 +0200

    Changed amount selector to hold item, not its name

--- frontend/tools/amount_selector.py
+++ frontend/tools/amount_selector.py
@@ -1,16 +1,16 @@
 import tkinter as tk
 from pathlib import Path
 class AmountSelector(tk.Frame):
-    def __init__(self, parent, item_name):
+    def __init__(self, parent, item):
         super().__init__(parent)
         self.minus_image = tk.PhotoImage(file = ".\images\minus.png")
         self.plus_image = tk.PhotoImage(file = ".\images\plus.png")
-        self.item_name = tk.Label(self, borderwidth=1, text=item_name, font = ("Courier", 15))
+        self.item = item
+        self.item_name = tk.Label(self, borderwidth=1, text=item.name, font = ("Courier", 15))
         self.subtract_button = tk.Button(self, image=self.minus_image, command = self.subtract_one)
         self.entry = tk.Entry(self, font = ("Courier", 35), width=5)
         self.add_button = tk.Button(self, image = self.plus_image, command = self.add_one)
         self.entry.insert(0, "0")
-
         self.item_name.grid(column=0, row=0)
         self.subtract_button.grid(column=1, row=0)
         self.entry.grid(column=2, row=0)
