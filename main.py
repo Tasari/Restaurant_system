f564f3e34f0c6e94aaee9a5bd8cbc46d6e57654a
Tasari <Tasari@wp.pl>
2020-05-19 17:12:48 +0200

    Finished adding new order

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from frontend.login import log_in
 from frontend.main_menu import main_menu
 from wallet import Wallet
+from tables import order
 user = log_in()
 print(user)
 wallet = Wallet(99990)
-main_menu(wallet, user)
+main_menu(user, wallet)
